2d0288189b74ba9992ae47c3329399f7a9600fb8
Aakash-2016 <aakash.gholap21@gmail.com>
2024-07-24 08:57:43 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("dice")
+    basic.showNumber(randint(1, 6))
+})
